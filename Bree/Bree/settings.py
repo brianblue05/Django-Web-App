d49d29cc22e4b22520b550b8d3596280c3b68a9b
Brian Mawira <58627423+brianblue05@users.noreply.github.com>
2022-09-15 00:52:22 +0300

    Update settings.py

--- Bree/Bree/settings.py
+++ Bree/Bree/settings.py
@@ -122,12 +122,6 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-STATICFILES_DIRS = [
-    "/home/special.Bree.com/Bree/static",
-    "/home/pages.com/pages/static",
-    "/opt/webfiles/common",
-]
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
